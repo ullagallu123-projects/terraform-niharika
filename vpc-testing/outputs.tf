27a3e2ba6716563f520b4e0446a31799051388e5
siva <sivaram0434@gmail.com>
2025-01-11 10:36:13 +0530

    updathe the config

--- vpc-testing/outputs.tf
+++ vpc-testing/outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
   value = module.testing.vpc_id
 }
-output "public_subnet_cidr" {
-  value = module.testing.public_subnet_cidr
+output "public_subnet_id" {
+  value = module.testing.public_subnet_id
 }
-output "private_subnet_cidr" {
-  value = module.testing.private_subnet_cidr 
+output "private_subnet_id" {
+  value = module.testing.private_subnet_id
 }
-output "db_subnet_cidr" {
-  value = module.testing.db_subnet_cidr 
+output "db_subnet_id" {
+  value = module.testing.db_subnet_id
 }
 output "db_subnet_group_name" {
-  value = module.testing.db_subnet_group_name
+  value = module.testing.db_subnet_group
 }
